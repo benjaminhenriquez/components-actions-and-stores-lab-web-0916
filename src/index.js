b1276f60569380ad1953d8f93f114d7e659559dc
github username <github email address>
2016-11-22 13:57:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from './store';
+import bandReducer from './reducers/band_reducer'
+import BandInput from './components/band_input_component.js'
+const store = createStore(bandReducer)
+
+const renderApp = () => {
+  ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
+}
+
+store.subscribe(renderApp);
+store.dispatch({})
+
 
 ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
-
 require('../test/index-test.js'); // Leave this in!
+
+ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
--- src/reducers/band_reducer.js
+++ src/reducers/band_reducer.js
@@ -1,1 +1,10 @@
+const bandInputComponent = (state=[], action) => {
+  switch (action.type) {
+  case 'ADD_BAND':
+    return [...state, action.payload];
+  default:
+    return state;
+  }
+}
 
+export default bandInputComponent
